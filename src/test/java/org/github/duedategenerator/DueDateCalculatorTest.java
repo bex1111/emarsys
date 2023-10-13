3949c04fa6839bc4df45da370249313131ca52a2
bex1111 <ba960919@gmail.com>
2023-10-13 19:22:33 +0200

    Initial commit

---
+++ src/test/java/org/github/duedategenerator/DueDateCalculatorTest.java
@@ -0,0 +1,14 @@
+package org.github.duedategenerator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DueDateCalculatorTest {
+
+    @Test
+     void test(){
+            assertThat(true).isTrue();
+    }
+
+}
